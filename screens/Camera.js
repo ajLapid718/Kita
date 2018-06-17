1636b94dd2ac3b24aaaa7fe35b202178ab7ba8eb
David <davidgcastro93@gmail.com>
2018-06-17 15:49:33 -0400

    finished translation

--- screens/Camera.js
+++ screens/Camera.js
@@ -4,7 +4,7 @@ import { Button, Text } from 'native-base';
 import { Camera, Permissions } from 'expo';
 import Loader from './Loader';
 import config from '../config';
-
+import axios from 'axios';
 class rootCamera extends React.Component {
   constructor() {
     super();
@@ -26,21 +26,23 @@ class rootCamera extends React.Component {
     if (this.camera) {
       let photo;
       let textRecieved;
+      let translatedText;
       try {
         photo = await this.camera.takePictureAsync({ base64: true });
         textRecieved = await this.getText(photo.base64);
+        translatedText = await this.getTranslatedText(textRecieved);
+        console.log(translatedText);
         this.setState({ loading: false });
       } catch (err) {
         console.log(err);
       }
-      navigate('rootText', { text: textRecieved });
+      navigate('rootText', { text: translatedText });
     }
   };
 
   getText = image => {
-    return fetch(config.googleCloudVision.api + config.apiKey, {
-      method: 'POST',
-      body: JSON.stringify({
+    return axios
+      .post(config.googleCloudVision.api + config.apiKey, {
         requests: [
           {
             image: {
@@ -55,43 +57,37 @@ class rootCamera extends React.Component {
           }
         ]
       })
-    })
-      .then(response => {
-        return response.json();
-      })
-      .then(text => {
-        return text.responses[0].fullTextAnnotation.text;
-      })
-      .then(parsedText => {
-        let fromLang = 'en';
-        let toLang = 'es';
-        let text = parsedText;
-        let url = `https://translation.googleapis.com/language/translate/v2`;
-        url += '&q=' + encodeURI(text);
-        url += `&target${toLang}`;
-        url += `&source=${fromLang}`;
-        url += `&key=${config.apiKey}`;
-
-        fetch(url, {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-            Accept: 'application/json'
-          }
-        })
-          .then(res => res.json())
-          .then(response => {
-            console.log(response);
-            return response;
-          })
-          .catch(error => {
-            console.log(
-              'There was an error with the translation request: ',
-              error
-            );
-          });
-      })
+      .then(response => response.data)
+      .then(text => text.responses[0].fullTextAnnotation.text)
       .catch(err => console.log(err));
+  };
+
+  getTranslatedText = parsedText => {
+    let fromLang = 'en';
+    let toLang = 'es';
+    let text = parsedText;
+
+    const API_KEY = config.apiKey;
+
+    let url = `https://translation.googleapis.com/language/translate/v2?key=${API_KEY}`;
+    url += '&q=' + encodeURI(text);
+    url += `&source=${fromLang}`;
+    url += `&target=${toLang}`;
+
+    return fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json'
+      }
+    })
+      .then(res => res.json())
+      .then(response => {
+        return response.data.translations[0].translatedText;
+      })
+      .catch(error => {
+        console.log('There was an error with the translation request: ', error);
+      });
   };
 
   render() {
